ecc7b4919636a3f38694e54c27cde6a346ee48d7
niveshkothari <niveshkothari395@gmail.com>
2025-07-16 18:37:44 +0530

    Fix case sensitivity for Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react"
-import Navbar from "./components/navbar/navbar"
+import Navbar from "./components/navbar/Navbar"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import LoginSignUp from "./Pages/LoginSignUp"
 import Shop from "./Pages/Shop";
